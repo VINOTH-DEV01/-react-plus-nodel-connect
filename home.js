3aba059a1740770f7a0ae24ae58ed21b26194021
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-12 23:46:07 +0530

    front enf d code filder is added res is render from UI side

--- home.js
+++ home.js
@@ -5,7 +5,22 @@ const router = express.Router();
 // Open the connection to the server
 router.get('/user',  (req, res) => {
     res.status(200).json({
-        message:["Anil","Bangalore"]
+        message:[{
+            name: "Anil",
+            location: "Bangalore",
+        },
+        {
+            name: "Anil2",
+            location: "Bangalore",
+        },
+        {
+            name: "Anil3",
+            location: "Bangalore",
+        },
+        {
+            name: "Anil4",
+            location: "Bangalore",
+        }]
     });
 })
 module.exports = router;
