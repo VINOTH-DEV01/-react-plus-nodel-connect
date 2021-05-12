1a5f1bfb98e9a5a264c638eec9f528171d00c93b
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-12 23:50:23 +0530

    front enf d code filder is added res is render from UI side - 2

--- home.js
+++ home.js
@@ -25,3 +25,52 @@ router.get('/user',  (req, res) => {
 })
 module.exports = router;
      
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// import React, { useState,  useEffect }  from 'react';
+
+// function App() {
+//   const [ data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch('/user').then((res) => {
+//       console.log(res);
+//         if(res.ok) {
+//          return res.json();
+//         }
+//     }).then(jsonres => {
+//       console.log("jsonres", jsonres);
+//       setData(jsonres.message)
+//     });
+//   });
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         {data.map((item, index) => {
+//           return (
+//             <p> {item.name +" "+ item.location}</p>
+//           )
+//         })}   
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >  
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
